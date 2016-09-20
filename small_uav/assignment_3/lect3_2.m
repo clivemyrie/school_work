fb737e0a4c55ca2c100c61d7b5e9e50e367e8733
Clive Myrie <clive.myrie@jhuapl.edu>
2016-09-19 21:54:56 -0400

    (1) Adds grid lines to m scripts (2) Adds new plot 3a

--- small_uav/assignment_3/lect3_2.m
+++ small_uav/assignment_3/lect3_2.m
@@ -3,9 +3,13 @@ function lect3_2
 close all
 subplot(3,1,1)
 plot(out.time_s, out.roll_deg); xlabel('time, s'); ylabel('roll, deg')
+grid on
+title('Problem 2')
 subplot(3,1,2)
 plot(out.time_s, out.pitch_deg); xlabel('time, s'); ylabel('pitch, deg')
+grid on
 subplot(3,1,3)
 plot(out.time_s, out.yaw_deg); xlabel('time, s'); ylabel('yaw, deg')
+grid on
 
 end
--- small_uav/assignment_3/lect3_3.m
+++ small_uav/assignment_3/lect3_3.m
@@ -2,6 +2,7 @@ function lect3_3
 %%
 close all;
 plot(out.north_m, out.alt_m); xlabel('north, m'); ylabel('alt, m')
+grid on
 title('Problem 3, Part A')
 max_alt = max(out.alt_m)
 idx = abs(out.alt_m) < 0.1;
@@ -9,6 +10,10 @@ north_pos_at_impact = out.north_m(idx)
 
 min_ground_speed = min(out.groundspeed_mps)
 max_ground_speed = max(out.groundspeed_mps)
+
+figure
+plot(out.time_s, out.alt_m); xlabel('time, s'); ylabel('alt, m')
+grid on
 
 
 end
